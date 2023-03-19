16ab38afa855ff71f7ecb9802a1dd13c906087fe
Mohammed <maxammed_1989@hotmail.com>
2023-03-19 10:10:41 +0100

    Initial commit

---
+++ src/main/java/com/footballSystem/footballSystem/FootballSystemApplication.java
@@ -0,0 +1,13 @@
+package com.footballSystem.footballSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/footballSystem/footballSystem/FootballSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballSystem.footballSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
